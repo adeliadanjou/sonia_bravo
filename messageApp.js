7c6b3d7fdf999e8cc99dd4837db8507caf6db54e
Adelia d Anjou <princessadelia@gmail.com>
2019-01-09 11:01:32 +0100

    changed url

--- messageApp.js
+++ messageApp.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 let messageApp = function(destination,body) {
-  return axios.post('http://exercise-1_messageapp_1:3000/message', {destination, body})
+  return axios.post('http://sonia_bravo_messageapp_1:3000/message', {destination, body})
 }
 
 module.exports = messageApp;
