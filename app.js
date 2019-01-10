b860937930310d9beb2e57a950d16383dd81515d
Adelia d Anjou <princessadelia@gmail.com>
2019-01-10 12:52:32 +0100

    settimeout de mongo establecido

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
-
+const mongoose     = require('mongoose');
 const indexRoute = require('./routes/index');
 const postMessageRoute = require('./routes/postMessage')
+
+// setTimeout(function(){ mongoose
+//   .connect('mongodb://sonia_bravo_mongo_1/cabify', {useNewUrlParser: true})
+//   .then(x => {
+//     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+//   })
+//   .catch(err => {
+//     console.error('Error connecting to mongo', err)
+//   }); }, 10000);
+
+  setTimeout(function(){
+    mongoose
+        .connect('mongodb://sonia_bravo_mongo_1/cabify', { useNewUrlParser: true })
+        .then(x => {
+            console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+        })
+        .catch(err => {
+            console.error('Error connecting to mongo', err)
+        });
+  }, 25000);
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
