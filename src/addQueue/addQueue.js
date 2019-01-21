1ca9b12a901774ab784b21f7a168233256746ee2
Adelia d Anjou <princessadelia@gmail.com>
2019-01-21 09:11:45 +0100

    changing queue

--- src/addQueue/addQueue.js
+++ src/addQueue/addQueue.js
@@ -8,6 +8,42 @@ const pay = require('../clients/pay')
 const messageQueue = new Queue('messageQueue');
 const uuidv1 = require('uuid/v1');
 
+messageQueue.process(function(job,done){
+
+  const myId = job.data.myId;
+  const destination = job.data.destination;
+  const body = job.data.body;
+ 
+   messageApp(myId, destination, body)
+   .then(resp => {
+ 
+     let status = "OK"
+     messageSave(myId, destination, body, status)
+     .then(pay()) 
+     .catch(console.log("aloha"))
+ 
+   })
+   .catch(resp => {
+ 
+     if (resp.status === undefined) {
+       let status = "TIMEOUT"
+       messageSave(myId, destination, body, status)
+    
+       console.log('Oh oh! Timeout!!!!')
+     } else {
+       let status = "NO ENVIADO"
+       messageSave(myId, destination, body, status)
+ 
+       console.log('Algo ocurrió patrón! Send Again')
+     }
+ 
+   })
+   .then(done())
+   .catch(done());
+ 
+ })
+
+
 
 let addToQueue = function(req,res,next) { 
 
@@ -18,49 +54,8 @@ const messageObj = {
   status: "PENDING",
 }
 
+pendingMessageSave(messageObj)
 messageQueue.add(messageObj)
-.then(
-  pendingMessageSave(messageObj)
-)
-.catch(
-  console.log("algo ha ido mal en addQueue")
-)
-
-
-messageQueue.process(function(job,done){
-
- const myId = job.data.myId;
- const destination = job.data.destination;
- const body = job.data.body;
-
-  messageApp(myId, destination, body)
-  .then(resp => {
-
-    let status = "OK"
-    messageSave(myId, destination, body, status)
-    .then(pay()) 
-    .catch(console.log("aloha"))
-
-  })
-  .catch(resp => {
-
-    if (resp.status === undefined) {
-      let status = "TIMEOUT"
-      messageSave(myId, destination, body, status)
-   
-      console.log('Oh oh! Timeout!!!!')
-    } else {
-      let status = "NO ENVIADO"
-      messageSave(myId, destination, body, status)
-
-      console.log('Algo ocurrió patrón! Send Again')
-    }
-
-  })
-  .then(done())
-  .catch(done());
-
-})
 
   .then(res.send(`processing your message ${messageObj.myId}`))
 .catch(res.send(`Your message with ${messageObj.myId} was not sent`));
