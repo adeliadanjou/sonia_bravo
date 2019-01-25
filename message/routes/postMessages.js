3cd2aaf3d15419a3b8929295d12d5ac56307f241
Adelia d Anjou <princessadelia@gmail.com>
2019-01-25 16:10:01 +0100

    postMessages refactorized

--- message/routes/postMessages.js
+++ message/routes/postMessages.js
@@ -4,12 +4,43 @@ const validation = require('../validations/validations')
 // const creditValidation = require('../../credit/validations/creditValidation')
 
 postMessageRoute.post('/messages', (req, res, next) => {
-    
-    validation(req,res)
-    // message = prepareMessage(req.body)
-    // enqueueToCretit(message)
-    // res(oK)
+
+    if (validation(req, res)) {
+        messageObj = prepareMessage(req);
+        isQueueLengthOk(res) ? pendingMessageSave(messageObj).then(() => {
+            creditQueue.add(messageObj)
+            res.send(`processing your message ${messageObj.myId}`)
+        }) : console.log("Cola desactivada"), res.send('Many connections, please try later!')
+
+    } else {
+        console.log("Not valid destination or body")
+    }
 });
+
+
+function prepareMessage(req) {
+    const myId = uuidv4()
+    // creo el objeto a añadir a add:   
+    return {
+        type: "Check Credit",
+        myId: myId,
+        destination: req.body.destination,
+        body: req.body.body,
+        status: "PENDING",
+    }
+}
+
+function isQueueLengthOk() {
+    let recovery = true;
+
+    return creditQueue.count()
+        .then(queuecount => {
+            if (queuecount > 6) { recovery = false }
+            if (queuecount > 10 && recovery === false || queuecount < 10 && recovery === false) {
+                return false
+            } else { return true }
+        })
+}
 
 
 
--- message/validations/validations.js
+++ message/validations/validations.js
@@ -4,7 +4,7 @@ const {
   creditQueue
 } = require('../messageQueue/messageQueue');
 
-let validation = function (req, res) {
+let validation = function (req) {
 
   const {
     destination,
@@ -13,42 +13,16 @@ let validation = function (req, res) {
   } = req.body;
 
   if (typeof destination !== "string" || typeof body !== "string") {
-
-    console.log("Please enter valid strings in both areas");
+    return false
 
   } else if (destination === "" || body === "") {
+    return false
 
-    console.log("Destination or body cannot be empty");
   } else {
-     
-  
-    const myId = uuidv4()
-    // creo el objeto a añadir a add:   
-    const messageObj = {
-      type: "Check Credit",
-      myId: myId,
-      destination: req.body.destination,
-      body: req.body.body,
-      status: "PENDING",
-    }
 
-    let recovery = true;
+    return true
 
-    creditQueue.count()
-    .then(queuecount =>  {
-     
-      if(queuecount > 6){recovery = false}
-
-    if(queuecount >10 && recovery === false || queuecount <10 && recovery === false)
-    {console.log("cola desactivada"),res.send('Many connections, please try later!')}
-    else {
-    pendingMessageSave(messageObj).then(() => {
-      creditQueue.add(messageObj)
-      res.send(`processing your message ${messageObj.myId}`)
-    })
   }
-})
+}
 
-  }}
-  
 module.exports = validation
