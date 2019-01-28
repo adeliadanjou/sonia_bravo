073a01762fb0ae992e8d3318556a29d948377c1e
Adelia d Anjou <princessadelia@gmail.com>
2019-01-28 18:24:20 +0100

    added to dockerfile grafana &prometheus

--- message/routes/getHealth.js
+++ message/routes/getHealth.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const getHealth = express.Router();
-const logger = require('../logs/winston')
+// const logger = require('../logs/winston')
 
-getHealth.get('/health', (res) => {
+getHealth.get('/health', (req,res,next) => {
+  // logger.info("Health of Haproxy OK!")
   res.status(200).json("alive")
-  logger.info("Health of Haproxy OK!")
 })
 
 module.exports = getHealth
